cbcd436227f5865ecf6dc6ac01d5b316daa0be9e
quintin <2449164582@qq.com>
2026-09-14 17:50:42 +0800

    feat(core): add pluggable allocator

--- include/mcpkit/core/types.h
+++ include/mcpkit/core/types.h
@@ -13,4 +13,16 @@ typedef struct mcp_tool mcp_tool_t;
 typedef struct mcp_resource mcp_resource_t;
 typedef struct mcp_prompt mcp_prompt_t;
 
+// Pluggable allocator. Any NULL function pointer falls back to libc
+// at context creation time. userdata is passed through to each call.
+typedef struct mcp_allocator {
+    void *(*malloc_fn)(size_t size, void *userdata);
+    void (*free_fn)(void *ptr, void *userdata);
+    void *(*calloc_fn)(size_t nmemb, size_t size, void *userdata);
+    void *(*realloc_fn)(void *ptr, size_t size, void *userdata);
+    void *userdata;
+} mcp_allocator_t;
+
+const mcp_allocator_t *mcp_default_allocator(void);
+
 #endif
---
+++ src/core/allocator.c
@@ -0,0 +1,35 @@
+#include "mcpkit/core/types.h"
+
+#include <stdlib.h>
+
+static void *libc_malloc(size_t size, void *userdata) {
+    (void)userdata;
+    return malloc(size);
+}
+
+static void libc_free(void *ptr, void *userdata) {
+    (void)userdata;
+    free(ptr);
+}
+
+static void *libc_calloc(size_t nmemb, size_t size, void *userdata) {
+    (void)userdata;
+    return calloc(nmemb, size);
+}
+
+static void *libc_realloc(void *ptr, size_t size, void *userdata) {
+    (void)userdata;
+    return realloc(ptr, size);
+}
+
+static const mcp_allocator_t k_default = {
+    .malloc_fn = libc_malloc,
+    .free_fn = libc_free,
+    .calloc_fn = libc_calloc,
+    .realloc_fn = libc_realloc,
+    .userdata = NULL,
+};
+
+const mcp_allocator_t *mcp_default_allocator(void) {
+    return &k_default;
+}
